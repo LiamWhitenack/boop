8d316932da0abdaaef963489c433258de6bd4fbe
LiamWhitenack <liamwhitenack@outlook.com>
2023-06-15 12:20:30 -0400

    added upgradePieces function

--- lib/player.dart
+++ lib/player.dart
@@ -5,6 +5,19 @@ class Player {
   List<Cat> cats = [];
   final String name;
   Player(this.name) {
-    kittens = List.filled(8, Kitten(name), growable: true);
+    kittens = List.filled(8, Kitten(this), growable: true);
+  }
+
+  void upgradePieces(List pieces) {
+    int numberOfPiecesPlayerHas = kittens.length + cats.length;
+    for (var piece in pieces) {
+      kittens.remove(piece);
+      cats.remove(piece);
+    }
+    if (numberOfPiecesPlayerHas - 3 != kittens.length + cats.length) {
+      throw Exception('something is wrong with the removal of pieces. \nThe kittens: $kittens \nThe cats: $cats');
+    }
+
+    cats.addAll(List.filled(3, Cat(this)));
   }
 }
