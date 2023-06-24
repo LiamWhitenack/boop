6821770b4ba3fcdba363e199a0f1cae7374e862f
LiamWhitenack <liamwhitenack@outlook.com>
2023-06-24 13:10:47 -0400

    added keys for sorting just because

--- lib/view_possibilities_screen.dart
+++ lib/view_possibilities_screen.dart
@@ -1,6 +1,7 @@
 import 'package:boop/grid.dart';
 import 'package:boop/possibility.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ViewPossibilitiesScreen extends StatefulWidget {
   final List<Possibility> possibilities;
@@ -40,6 +41,7 @@ class _ViewPossibilitiesScreenState extends State<ViewPossibilitiesScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final focusNode = FocusNode();
     Possibility mainPossibility = widget.possibilities[index];
     return Scaffold(
       appBar: AppBar(
@@ -50,79 +52,94 @@ class _ViewPossibilitiesScreenState extends State<ViewPossibilitiesScreen> {
           style: TextStyle(fontSize: 25),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 500,
-                width: 500,
-                child: Grid(
-                  board: mainPossibility.futureBoard,
-                  playerOne: mainPossibility.player,
-                  playerTwo: mainPossibility.otherPlayer,
-                  refreshMyGamePageState: widget.refreshMyGamePageState,
-                  alternatePlayerOneTurn: widget.alternatePlayerOneTurn,
-                  playerOneTurn: widget.playerOneTurn,
-                  winner: widget.winner,
+      body: RawKeyboardListener(
+        focusNode: focusNode,
+        onKey: (event) {
+          if (event.isKeyPressed(LogicalKeyboardKey.arrowLeft)) {
+            if (event is RawKeyDownEvent) {
+              decreaseIndex();
+            }
+          }
+          if (event.isKeyPressed(LogicalKeyboardKey.arrowRight)) {
+            if (event is RawKeyDownEvent) {
+              increaseIndex();
+            }
+          }
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 500,
+                  width: 500,
+                  child: Grid(
+                    board: mainPossibility.futureBoard,
+                    playerOne: mainPossibility.player,
+                    playerTwo: mainPossibility.otherPlayer,
+                    refreshMyGamePageState: widget.refreshMyGamePageState,
+                    alternatePlayerOneTurn: widget.alternatePlayerOneTurn,
+                    playerOneTurn: widget.playerOneTurn,
+                    winner: widget.winner,
+                  ),
                 ),
-              ),
-              const SizedBox(width: 100),
-              Text(
-                  'Orange Kittens: ${mainPossibility.otherPlayer.kittens.length}\nOrange Cats: ${mainPossibility.otherPlayer.cats.length}\nGrey Kittens: ${mainPossibility.player.kittens.length}\nGrey Cats: ${mainPossibility.player.cats.length}\n\nScore: ${mainPossibility.score}'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              index != 0
-                  ? TextButton(
-                      onPressed: () {
-                        decreaseIndex();
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blue),
-                          borderRadius: const BorderRadius.all(Radius.circular(20)),
-                        ),
-                        height: 50,
-                        width: 75,
-                        // color: Colors.blue,
-                        child: const Icon(
-                          Icons.arrow_left_outlined,
-                          color: Colors.blue,
-                        ),
-                      ),
-                    )
-                  : const SizedBox(width: 75),
-              const SizedBox(width: 50, height: 50),
-              index != widget.possibilities.length - 1
-                  ? TextButton(
-                      onPressed: () {
-                        increaseIndex();
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blue),
-                          borderRadius: const BorderRadius.all(Radius.circular(20)),
-                        ),
-                        height: 50,
-                        width: 75,
-                        // color: Colors.blue,
-                        child: const Center(
-                          child: Icon(
-                            Icons.arrow_right_outlined,
+                const SizedBox(width: 100),
+                Text(
+                    'Orange Kittens: ${mainPossibility.otherPlayer.kittens.length}\nOrange Cats: ${mainPossibility.otherPlayer.cats.length}\nGrey Kittens: ${mainPossibility.player.kittens.length}\nGrey Cats: ${mainPossibility.player.cats.length}\n\nScore: ${mainPossibility.score}'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                index != 0
+                    ? TextButton(
+                        onPressed: () {
+                          decreaseIndex();
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                            borderRadius: const BorderRadius.all(Radius.circular(20)),
+                          ),
+                          height: 50,
+                          width: 75,
+                          // color: Colors.blue,
+                          child: const Icon(
+                            Icons.arrow_left_outlined,
                             color: Colors.blue,
                           ),
                         ),
-                      ),
-                    )
-                  : const SizedBox(width: 75),
-            ],
-          ),
-        ],
+                      )
+                    : const SizedBox(width: 75),
+                const SizedBox(width: 50, height: 50),
+                index != widget.possibilities.length - 1
+                    ? TextButton(
+                        onPressed: () {
+                          increaseIndex();
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                            borderRadius: const BorderRadius.all(Radius.circular(20)),
+                          ),
+                          height: 50,
+                          width: 75,
+                          // color: Colors.blue,
+                          child: const Center(
+                            child: Icon(
+                              Icons.arrow_right_outlined,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                      )
+                    : const SizedBox(width: 75),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
